b2234e26b34f5550433d8e1b674396d3ee5d1d3c
Wertual <wertual08@gmail.com>
2021-08-24 11:52:00 +0300

    Working on ci-cd pipeline.

--- project/Responses/AdminProductResponse.cs
+++ project/Responses/AdminProductResponse.cs
@@ -8,7 +8,7 @@ namespace Api.Responses {
         public decimal Price { get; init; }
         public string Name { get; init; }
         public string Description { get; init; }
-        public DateTime CreatedAt { get; init; }
+        public DateTime CreatedAt { get; init; } 
         public DateTime UpdatedAt { get; init; }
         public List<string> Records { get; init; }
         public List<AdminCategoryResponse> Categories { get; init; }
