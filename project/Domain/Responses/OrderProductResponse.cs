50878ed0460ee48e715048917147e233355926da
Wertual <wertual08@gmain.com>
2021-10-24 15:58:31 +0300

    OrderProductResponse now contains LiteProductResponse instead of ProductResponse.

--- project/Domain/Responses/OrderProductResponse.cs
+++ project/Domain/Responses/OrderProductResponse.cs
@@ -4,12 +4,12 @@ namespace Api.Domain.Responses {
     public record OrderProductResponse {
         public int Amount { get; init; }
         public decimal Price { get; init; }
-        public ProductResponse Product { get; init; }
+        public LiteProductResponse Product { get; init; }
     
         public OrderProductResponse(OrderProduct model) {
             Amount = model.Amount;
             Price = model.Price;
-            Product = new ProductResponse(model.Product);
+            Product = new LiteProductResponse(model.Product);
         }
     }
 }
