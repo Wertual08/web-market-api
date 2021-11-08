e459e38eb8a4214b93eb56c1500ec387f92b5cd0
Wertual <wertual08@gmail.com>
2021-11-08 20:42:29 +0300

    Constraints for text fields in ReviewCreateRequest added.

--- project/Domain/Requests/ReviewCreateRequest.cs
+++ project/Domain/Requests/ReviewCreateRequest.cs
@@ -4,13 +4,13 @@ namespace Api.Domain.Requests {
     public class ReviewCreateRequest {
         [Range(1, 5)]
         public int? Grade { get; set; }
-        [Required]
+        [Required, MaxLength(25)]
         public string Name { get; set; }
         [Required, EmailAddress]
         public string Email { get; set; }
-        [Required]
+        [Required, MaxLength(40)]
         public string Address { get; set; }
-        [Required]
+        [Required, MaxLength(256)]
         public string Description { get; set; }
     }
 }
